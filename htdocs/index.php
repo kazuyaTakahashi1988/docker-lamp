3a286d0e85a8ea49f1c386635cc13e4759ecb57b
kazuyaTakahashi1988 <kazuya.takahashi.631130@gmail.com>
2022-05-09 17:03:49 +0900

    SET

--- htdocs/index.php
+++ htdocs/index.php
@@ -7,7 +7,11 @@
 </ul>
 <ul>
     <li><a href="http://localhost:3000/">phpMyAdmin</a><br>
-    user：root<br>pass：rootpwd@12345<br><br>
-   <small>★ WPなど導入時、DBホスト名は localhost でなく php72-apache-mysql をお使いください（これは container_name なので任意のものに変更可） ★ </small>
-</li>
+        user：root<br>pass：rootpwd@12345<br><br>
+        <small>★ WPなど導入時、DBホスト名は localhost でなく <strong>php73-apache-mysql</strong> をお使いください（これは container_name なので任意のものに変更可） ★ </small>
+        <br>
+        <small>★ バーチャルホスト（及びサイトディレクトリ）を増やしたい時は  <strong>docker-compose.yml</strong> & <strong>.webserver/httpd.conf</strong> を編集後、コンテナを再起動してください（ docker-compose up -d --build コマンド ） ★ </small>
+        <br>
+        <small>★ デフォルトでdockerホストにWP.ver5.8.1がDLされますが使わないようでしたら消してしまって構いません（あくまでテストに用いるものなので） ★ </small>
+    </li>
 </ul>
