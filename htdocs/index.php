aa13f4f36c18c519032f5a9c258ba0b9de0c8ddc
kazuyaTakahashi1988 <57883262+kazuyaTakahashi1988@users.noreply.github.com>
2022-05-09 10:11:13 +0900

    SET

---
+++ htdocs/index.php
@@ -0,0 +1,13 @@
+# http://localhost
+
+<ul>
+    <li><a href="http://example-site1.jp.localhost/">example-site1.jp.localhost</a></li>
+    <li><a href="http://example-site2.jp.localhost/">example-site2.jp.localhost</a></li>
+    <li><a href="http://example-site3.jp.localhost/">example-site3.jp.localhost</a></li>
+</ul>
+<ul>
+    <li><a href="http://localhost:3000/">phpMyAdmin</a><br>
+    user：root<br>pass：rootpwd@12345<br><br>
+   <small>★ WPなど導入時、DBホスト名は localhost でなく php72-apache-mysql をお使いください（これは container_name なので任意のものに変更可） ★ </small>
+</li>
+</ul>
